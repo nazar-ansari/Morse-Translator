771658a84d157d7e4bbcda0ac6981bb8738143d4
Nazar Ansari <nazaransari06@gmail.com>
2022-07-15 20:06:12 +0530

    Added Methods to hide and Display Elements

---
+++ src/script.js
@@ -0,0 +1,11 @@
+/*                                                      Function to Display Elements          */
+function fadeIn(elem) {
+  "use strict";
+  elem.style.display = "block";
+  elem.style.opacity = 0.9;
+}
+
+/*                                                      Function to HIde Elements          */
+function hide(elem) {
+  elem.style.display = "none";
+}
